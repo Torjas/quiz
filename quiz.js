b33e02b091d9df80a50499c49993d5a2f9f2c3b8
Camila San <camila@nand.io>
2017-08-07 23:06:53 +0200

    Added action to click on option.

--- quiz.js
+++ quiz.js
@@ -2,5 +2,8 @@ $(window).on('load', function(){
   // read quiz json
   // validate answers
   // display the follow up question and message
-
+  $('input').on('click', function(){
+     $('div').text('You clicked on ' + $(this).closest('li').text());
+     $('div').slideDown();
+  });
 });
